3fbdb6358e04e8ce438f1c0f06294a707cf467ca
herost <153154703@qq.com>
2019-10-13 22:04:33 +0800

    add getter setter

--- src/main/java/com/hh/spring_boot/Application.java
+++ src/main/java/com/hh/spring_boot/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@SpringBootApplication
 
+@SpringBootApplication
+@RestController
 public class Application {
 
     @RequestMapping("/")
@@ -16,6 +16,8 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
 
 }
 
--- src/main/java/com/hh/spring_boot/entity/Person.java
+++ src/main/java/com/hh/spring_boot/entity/Person.java
@@ -9,5 +9,19 @@ public class Person {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
